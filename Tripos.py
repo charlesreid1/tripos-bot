adb2e42f9bf192412a6a870eccf4f37163c216f9
Charles Reid <charlesreid1@gmail.com>
2018-03-04 03:29:43 +0000

    Fix up Tripos from production

--- Tripos.py
+++ Tripos.py
@@ -3,24 +3,19 @@ import logging
 import glob
 from PhotoADaySheep import PhotoADaySheep
 
-LOG_FILE = "tripos_bot.log"
-
 def setup():
     k = rmm.SimpleListKeymaker()
-    k.make_keys([0],'keys/')
+    k.make_keys([0],'/home/charles/codes/tripos-bot/keys/')
 
 def run():
-    sh = rmm.Shepherd('keys/',sheep_class=PhotoADaySheep)
+    sh = rmm.Shepherd('/home/charles/codes/tripos-bot/keys/',sheep_class=PhotoADaySheep)
 
     sh.perform_action('photo_a_day',{
-                      'upload':True,
-                      'publish':True,
+                      'upload'  : True,
+                      'publish' : True,
                       'images_dir':'latex'
                     })
 
 if __name__=="__main__":
-
     #setup()
-
     run()
-
