786e26914034ac13d4b6513d4aa5af4985b059b8
Charles Reid <charlesreid1@gmail.com>
2019-06-30 22:31:54 -0700

    fix typo in link stuff script

--- bot/latex/link_stuff.py
+++ bot/latex/link_stuff.py
@@ -5,8 +5,8 @@ last = 256
 maxx = 365
 
 for i in range(maxx-last):
-    source = "%03d.jpg"%(i+1)
-    target = "%03d.jpg"%(last+i+1)
+    source = "%03d.tex"%(i+1)
+    target = "%03d.tex"%(last+i+1)
     print(" ".join(['ln','-fs',source,target]))
     subprocess.call(['ln','-fs',source,target])
 
